f2965bb2b10b259b7de7121762c18c02e77c271e
Hayley Prior <hayley.prior@experian.com>
2019-11-28 17:13:05 +0000

    set up routing

--- ToDoList/src/app/app-routing.module.ts
+++ ToDoList/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { TaskListComponent } from './task-list/task-list.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/task_list', pathMatch: 'full' },
+  { path: 'task_list', component: TaskListComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
