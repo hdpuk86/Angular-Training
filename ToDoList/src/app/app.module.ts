7b024d313b9ee779e4bd582e34e481ddbc49bdab
Hayley Prior <hayley.prior@experian.com>
2019-11-28 17:04:40 +0000

    ng generate module app-routing --flat --module=app

--- ToDoList/src/app/app.module.ts
+++ ToDoList/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TaskListComponent } from './task-list/task-list.component';
     TaskListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
